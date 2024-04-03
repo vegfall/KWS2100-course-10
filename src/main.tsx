33b10e6f1a60e269eab1f08ff5d4b1cceec031ea
Vegard Falldalen <vegfall@gmail.com>
2024-04-03 13:04:08 +0200

    Added prettier, husky and tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root")!);
 
