5503198cf112b8f3af5b5d89a1c8a60274e361db
Vegard Falldalen <vegfall@gmail.com>
2024-04-03 16:30:54 +0200

    Refactor vehicle parsing code

--- src/components/application/application.tsx
+++ src/components/application/application.tsx
@@ -1,25 +1,63 @@
-import React, { MutableRefObject, useEffect, useRef } from "react";
+import React, {
+  MutableRefObject,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { Feature, Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
 import "ol/ol.css";
-import { FeedMessage } from "../../../generated/gtfs-realtime";
+import { FeedMessage, VehiclePosition } from "../../../generated/gtfs-realtime";
 import { Point } from "ol/geom";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 
 useGeographic();
 
-const vehicleSource = new VectorSource();
-const vehicleLayer = new VectorLayer({ source: vehicleSource });
-
+const backgroundLayer = new TileLayer({ source: new OSM() });
 const map = new Map({
   view: new View({ center: [10, 63], zoom: 8 }),
-  layers: [new TileLayer({ source: new OSM() }), vehicleLayer],
 });
 
+interface LiveVehicle {
+  routeId: string;
+  coordinate: number[];
+}
+
+function convertFromProtobuf(
+  vehicle: VehiclePosition | undefined,
+): LiveVehicle | undefined {
+  if (!vehicle) return;
+  const { position, trip } = vehicle;
+  if (!position || !trip) return;
+  const { longitude, latitude } = position;
+  const { routeId } = trip;
+  if (!routeId) return;
+
+  return {
+    routeId,
+    coordinate: [longitude, latitude],
+  };
+}
+
 export function Application() {
+  const [vehicleSource, setVehicleSource] = useState<VectorSource>();
+  const vehicleLayer = useMemo(
+    () => new VectorLayer({ source: vehicleSource }),
+    [vehicleSource],
+  );
+
+  const layers = useMemo(() => {
+    return [backgroundLayer, vehicleLayer];
+  }, [vehicleLayer]);
+
+  useEffect(() => {
+    map.setLayers(layers);
+  }, [layers]);
+
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
 
   async function fetchVehiclePosition() {
@@ -35,14 +73,19 @@ export function Application() {
       new Uint8Array(await res.arrayBuffer()),
     );
 
+    const vehicles: LiveVehicle[] = [];
+
     for (const { vehicle } of resMessage.entity) {
-      if (!vehicle) continue;
-      const { position } = vehicle;
-      if (!position) continue;
-      const { longitude, latitude } = position;
-      const point = new Point([longitude, latitude]);
-      vehicleSource.addFeature(new Feature(point));
+      const v = convertFromProtobuf(vehicle);
+
+      if (v) vehicles.push(v);
     }
+
+    setVehicleSource(
+      new VectorSource({
+        features: vehicles.map((v) => new Feature(new Point(v.coordinate))),
+      }),
+    );
   }
 
   useEffect(() => {
