9415691b95f123f261eb8e70921053cd96443eda
Vegard Falldalen <vegfall@gmail.com>
2024-04-03 16:48:03 +0200

    Now updating vehicles

--- src/components/application/useVehicles.tsx
+++ src/components/application/useVehicles.tsx
@@ -51,6 +51,8 @@ export function useVehicles() {
 
   useEffect(() => {
     fetchVehiclePosition();
+    const intervalId = setInterval(() => fetchVehiclePosition(), 15000);
+    return () => clearInterval(intervalId);
   }, []);
 
   return vehicles;
